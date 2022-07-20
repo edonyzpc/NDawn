eb705e55dea0e365f265b08c476a719f5bb603a1
edonyzpc <edonyzpc@yahoo.com>
2022-07-20 03:32:28 +0000

    fix: fix `yarn dev` run failed for package.json issue
    Signed-off-by: edonyzpc <edonyzpc@yahoo.com>

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ const pump = require('pump');
 
 // gulp plugins and utils
 const livereload = require('gulp-livereload');
-const gulpStylelint = require('gulp-stylelint');
 const postcss = require('gulp-postcss');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
@@ -31,7 +30,7 @@ function handleError(done) {
 
 function hbs(done) {
     pump([
-        src(['*.hbs', 'partials/**/*.hbs', 'members/**/*.hbs']),
+        src(['*.hbs', 'partials/**/*.hbs']),
         livereload()
     ], handleError(done));
 }
@@ -62,19 +61,6 @@ function js(done) {
     ], handleError(done));
 }
 
-function lint(done) {
-    pump([
-        src(['assets/css/**/*.css', '!assets/css/vendor/*']),
-        gulpStylelint({
-            fix: true,
-            reporters: [
-                {formatter: 'string', console: true}
-            ]
-        }),
-        dest('assets/css/')
-    ], handleError(done));
-}
-
 function zipper(done) {
     const filename = require('./package.json').name + '.zip';
 
@@ -90,13 +76,12 @@ function zipper(done) {
     ], handleError(done));
 }
 
-const hbsWatcher = () => watch(['*.hbs', 'partials/**/*.hbs', 'members/**/*.hbs'], hbs);
+const hbsWatcher = () => watch(['*.hbs', 'partials/**/*.hbs'], hbs);
 const cssWatcher = () => watch('assets/css/**/*.css', css);
 const jsWatcher = () => watch('assets/js/**/*.js', js);
 const watcher = parallel(hbsWatcher, cssWatcher, jsWatcher);
 const build = series(css, js);
 
 exports.build = build;
-exports.lint = lint;
 exports.zip = series(build, zipper);
 exports.default = series(build, serve, watcher);
